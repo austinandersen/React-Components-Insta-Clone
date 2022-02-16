508ff92b5e6da5fc09588e10b4d59bab244bc1c9
austin andersen <austinjamesgf@gmail.com>
2022-02-15 22:44:36 -0800

    completed tasks

--- src/App.js
+++ src/App.js
@@ -5,15 +5,22 @@
 */
 
 // Import the state hook
-import React from 'react';
+import React, { useState } from 'react';
 // Import the Posts (plural!) and SearchBar components, since they are used inside App component
+import Posts from './components/Posts/Posts';
+import SearchBar from './components/SearchBar/SearchBar';
 // Import the dummyData
+import dummyData from './dummy-data'
+
 import './App.css';
 
 const App = () => {
   // Create a state called `posts` to hold the array of post objects, **initializing to dummyData**.
   // This state is the source of truth for the data inside the app. You won't be needing dummyData anymore.
   // To make the search bar work (which is stretch) we'd need another state to hold the search term.
+  const [posts, setPosts] = useState(dummyData);
+  //never mutate state directly. example; posts = newPosts
+  //proper example; setPosts(newPosts)
 
   const likePost = postId => {
     /*
@@ -27,11 +34,25 @@ const App = () => {
         - if the `id` of the post matches `postId`, return a new post object with the desired values (use the spread operator).
         - otherwise just return the post object unchanged.
      */
+    //map -> loop over an array and do something to each element and return new array
+    //filter -> loop over an array and based on conditions return particular elements into new array
+    //reduce -> loop over array and reduce it to a singular(not necessarily singular) value
+    //find -> loop over array based on a condition return first element it finds that matches condition then stops running 
+    const updatedPosts = posts.map(post => {
+      if (post.id === postId) {
+        return { ...post, likes: post.likes + 1 };
+      }else {
+        return post;
+      }
+    })
+    setPosts(updatedPosts);
   };
 
   return (
     <div className='App'>
       {/* Add SearchBar and Posts here to render them */}
+      <SearchBar />
+      <Posts posts={posts} likePost={likePost} />
       {/* Check the implementation of each component, to see what props they require, if any! */}
     </div>
   );
